365eee29b500a8460d93e0bcca164047b4cf3690
FardaPc1 <tahashahrokh38@gmail.com>
2025-09-16 17:51:02 +0330

    add TransactionType to Transaction Models

--- payments/models.py
+++ payments/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
+
 # Create your models here.
 class Wallet(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wallet")
@@ -9,14 +10,42 @@ class Wallet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 class TransactionType(models.TextChoices):
     DEBIT = 'DEBIT', 'Debit'
     CREDIT = 'CREDIT', 'Credit'
 
+
 class Transaction(models.Model):
-    origin_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="outgoing_transactions")
-    destination_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="incoming_transactions")
-    transaction_type = models.CharField(max_length=10, choices=TransactionType.choices,default=TransactionType.DEBIT)
+    origin_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="outgoing_transactions",
+                                      null=True, blank=True)
+    destination_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="incoming_transactions",
+                                           null=True, blank=True)
+    transaction_type = models.CharField(max_length=10, choices=TransactionType.choices, default=TransactionType.DEBIT)
     amount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def debt_payment(self):
+        if not self.origin_wallet or not self.destination_wallet:
+            raise ValueError("Both origin and destination wallets are required for DEBIT transaction.")
+        if self.origin_wallet.balance < self.amount:
+            raise Exception('Please increase your balance first and then take action.')
+        self.origin_wallet.balance -= self.amount
+        self.destination_wallet.balance += self.amount
+        self.origin_wallet.save()
+        self.destination_wallet.save()
+
+    def credit_payment(self):
+        if not self.destination_wallet:
+            raise ValueError("Destination wallet is required for CREDIT transaction.")
+        self.destination_wallet.balance += self.amount
+        self.destination_wallet.save()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if self.transaction_type == TransactionType.DEBIT:
+                self.debt_payment()
+            else:
+                self.credit_payment()
+        super().save(*args, **kwargs)
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from decimal import Decimal
+from urllib import request
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from payments.models import TransactionType, Wallet, Transaction
+
 
 # Create your views here.
+@login_required
+def deduct_balance_on_booking(request):
+    if request.method == 'POST':
+        origin_wallet_id = request.POST.get('destination_wallet')
+        destination_wallet_id = request.POST.get('destination_wallet')
+
+        try:
+            origin_wallet = Wallet.objects.get(id=origin_wallet_id)
+            destination_wallet = Wallet.objects.get(id=destination_wallet_id)
+        except Wallet.DoesNotExist:
+            messages.error(request, "wallet not found")
+            return redirect("debit_wallet")
+
+        transaction_type = TransactionType.DEBIT
+        amount = request.POST.get('amount')
+
+        try:
+            amount = Decimal(amount)
+        except:
+            messages.error(request, "amount is not a valid number")
+            return redirect("debit_wallet")
+
+        description = f"Transfer {amount} Toman from account {origin_wallet} to account {destination_wallet}"
+
+        try:
+            Transaction.objects.create(
+                origin_wallet=origin_wallet,
+                destination_wallet=destination_wallet,
+                transaction_type=TransactionType.DEBIT,
+                amount=amount,
+                description=description
+            )
+            messages.success(request, "Transfer successful")
+        except ValueError as e:
+            messages.error(request, str(e))
+        return redirect("debit_wallet")
+
+    wallets = Wallet.objects.exclude(user=request.user)
+    return render(request, "payments/debit_wallet.html")
