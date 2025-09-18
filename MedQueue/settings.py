c90a7e7518a278f1853ee4dd1ce394f24752505d
ali <7731.aa@gmail.com>
2025-09-18 18:14:46 +0430

    add .env file for smtp config

--- MedQueue/settings.py
+++ MedQueue/settings.py
@@ -145,9 +145,9 @@ STORAGES = {
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "7731.aa@gmail.com"
-EMAIL_HOST_PASSWORD = "xpmi zvkz rgmz ywmc"
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import secrets
 
-# Create your models here.
 class CustomUser(AbstractUser):
     class RoleType(models.TextChoices):
         ADMIN = 'admin', 'Admin'
