0a4717367e257a6f2a8f9ba40ebad763673ede03
Pourya Enayati <pouryaenayati@gmail.com>
2025-09-22 01:23:15 +0330

    fix urls issue

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import available_slots, reserve_slot, my_reservations, AppointmentListView
+from .views import available_slots, reserve_slot, my_reservations, my_appointments
 
 urlpatterns = [
     path('available-slots/<int:doctor_id>/', available_slots, name='available-slots'),
     path('reserve-slot/<int:slot_id>/', reserve_slot, name='reserve-slot'),
     path('my-reservations/', my_reservations, name='my-reservations'),
-    path('api/appointments/', AppointmentListView.as_view(), name='appointments-api'),
+    path('api/appointments/', my_appointments, name='appointments-api'),
 ]
