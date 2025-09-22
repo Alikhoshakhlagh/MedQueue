3fd5757a85ab870cae531bcf9782bf1b5368d8f1
Mohammad Hosein <mhfathali2003@gmail.com>
2025-09-22 12:29:30 +0330

    register Review model in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "doctor", "rating", "created_at")
+    search_fields = ("user__username", "doctor__name", "comment")
+    list_filter = ("rating",)
